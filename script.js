372de08abbb9eedc02dee19cd7e4cf90e55ff961
PulkitMohan <pulkitmohan52@gmail.com>
2020-06-26 19:59:02 +0530

    responsive site

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width= '100px';
+    document.getElementById('side-menu').style.marginLeft= '400px';
+    document.getElementById('side-menu').style.marginTop= '65px';
+    document.getElementById('side-menu').style.height='250px';
+
+    console.log("button clicked");
+}
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width= '200px';
+}
